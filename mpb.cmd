d767c0c8813e52d801e134658b3dd3364bd8d7a1
Tam Ho <46585135+tamhq2@users.noreply.github.com>
2023-08-19 20:18:17 +0700

    Add files via upload

--- mpb.cmd
+++ mpb.cmd
@@ -39,13 +39,13 @@ echo.
 echo ==================================================================================
 echo "Enter a menu option in the Keyboard [1,2,3,4,5,6] :"
 choice /C:123456 /N
-if errorlevel 6 goto :Exit & cls & endlocal & goto :MainMenu
-if errorlevel 5 goto :Loadd & cls & endlocal & goto :MainMenu
-if errorlevel 4 goto :Load & cls & endlocal & goto :MainMenu
-if errorlevel 3 goto :VPN & cls & endlocal & goto :MainMenu
-if errorlevel 2 goto :Permission & cls & endlocal & goto :MainMenu
-if errorlevel 1 goto :Access & cls & endlocal & goto :MainMenu
-goto :MainMenu
+if errorlevel 6 goto :Exit & cls & endlocal & goto :Main
+if errorlevel 5 goto :Loadd & cls & endlocal & goto :Main
+if errorlevel 4 goto :Load & cls & endlocal & goto :Main
+if errorlevel 3 goto :VPN & cls & endlocal & goto :Main
+if errorlevel 2 goto :Permission & cls & endlocal & goto :Main
+if errorlevel 1 goto :Access & cls & endlocal & goto :Main
+goto :Main
 ::===============================================================================================================
 :Access
 taskkill /im chrome.exe >nul & del /s/q %temp% >nul 2>&1 && cleanmgr /sagerun:1 && start chrome --restore-last-session
